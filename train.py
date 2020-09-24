f862ed66cc210df0c8103125b1d75f80f045483f
Toshiki Watanabe <kuboy2482@gmail.com>
2020-09-24 20:23:07 +0900

    add num_steps to arguments

--- train.py
+++ train.py
@@ -36,16 +36,24 @@ def main(args):
         device=torch.device("cuda" if args.cuda else "cpu"),
         seed=args.seed,
     )
-    trainer = Trainer(env=env, env_test=env_test, algo=algo, log_dir=log_dir)
+    trainer = Trainer(
+        env=env,
+        env_test=env_test,
+        algo=algo,
+        log_dir=log_dir,
+        seed=args.seed,
+        num_steps=args.num_steps,
+    )
     trainer.train()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--num_steps", type=int, default=3 * 10 ** 6)
     parser.add_argument("--domain_name", type=str, default="cheetah")
     parser.add_argument("--task_name", type=str, default="run")
     parser.add_argument("--action_repeat", type=int, default=4)
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--cuda", action='store_true')
+    parser.add_argument("--cuda", action="store_true")
     args = parser.parse_args()
     main(args)
